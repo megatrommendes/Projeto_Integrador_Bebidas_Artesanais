e729ff013dfbea47202c5efdd78709b5393edef5
Marcio <108750178+megatrommendes@users.noreply.github.com>
2023-12-04 18:41:36 -0300

    Add files via upload

---
+++ recebedoisnumeroscomoparametro.js
@@ -0,0 +1,9 @@
+function operacoesBasicas(a, b) {
+  console.log(`${a} + ${b} = ${a + b}`);
+  console.log(`${a} - ${b} = ${a - b}`);
+  console.log(`${a} x ${b} = ${a * b}`);
+  console.log(`${a} / ${b} = ${a / b}`);
+}
+
+// Exemplo de uso da função com os números 4 e 5
+operacoesBasicas(4, 5);
